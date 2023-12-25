2f6cf35d7a39a224386db5ec0d6b8e957b22dacf
Luiz Augusto <luis2848@gmail.com>
2023-12-25 17:05:47 -0300

    Adicionando herois ao carrossel

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,4 +1,9 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 import HeroDetails from "../HeroDetails";
+import HeroPicture from "../HeroPicture";
 
 import styles from "./carousel.module.scss";
 
@@ -10,13 +15,38 @@ interface Iprops {
 }
 
 export default function Carousel({ heroes, activedId }: Iprops) {
+  const [visibleItems, setVisibleItems] = useState<IHeroData[] | null>(null);
+  const [activeIndex, setActiveIndex] = useState<number>(
+    heroes.findIndex((hero) => hero.id === activedId)
+  );
+
+  useEffect(() => {
+    //Formula para que não importa o número, está sempre dentro do Array
+    const indexInArrayScope =
+      ((activeIndex % heroes.length) + heroes.length) % heroes.length;
+
+    //Dividindo o array para exbir 3 herois
+    const visibleItems = [...heroes, ...heroes].slice(
+      indexInArrayScope,
+      indexInArrayScope + 3
+    );
+    setVisibleItems(visibleItems);
+  }, [heroes, activeIndex]);
+
+  if (!visibleItems) {
+    return null;
+  }
+
   return (
     <>
       <div className={styles.container}>
-
         <div className={styles.carousel}>
           <div className={styles.wrapper}>
-            Lista com os heróis
+            {visibleItems.map((item) => (
+              <div key={item.id} className={styles.hero}>
+                <HeroPicture hero={item} />
+              </div>
+            ))}
           </div>
         </div>
 
